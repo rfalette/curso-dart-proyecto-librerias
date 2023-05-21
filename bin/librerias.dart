4d5b9bec4c02cbf835e240d1d8b274982caf6d1f
Ramon <hfalette@outlook.com>
2023-05-21 16:34:12 -0400

    mi seguindo commit

--- bin/librerias.dart
+++ bin/librerias.dart
@@ -1,5 +1,28 @@
-import 'package:librerias/librerias.dart' as librerias;
+//import 'package:librerias/librerias.dart' as librerias;
+//import 'package:characters/characters.dart';
 
-void main(List<String> arguments) {
-  print('Hola Mundo: ${librerias.calculate()}!');
+void main(List<String> args) {}
+
+
+/*void main(List<String> arguments) {
+  //print('Hola Mundo: ${librerias.calculate()}!');
+  const texto = "Hola Mundo!👋, Que tal.";
+  //print(texto.characters.length);
+
+  //COMOCIENDO EL PAQUETE CHARACTERS
+
+  //Substring
+  //print(texto.substring(5, 13));
+  //print(texto.characters.skip(5).take(7).toString());
+  //Splitting
+  const text = "Hola👨‍👩‍👧‍👦con👧todos👧abc";
+  //print(text);
+  print(text.split("👧"));
+  print(text.characters.split("👧".characters).toList());
 }
+
+USO DE PAQUETES EN DART 
+para la descarga de paquetes en dart nos apoyamos de la 
+pagina web pub.dev que es una comunidad de dart que desarroyan
+paqueterias para dar soporte al lenguaje
+*/
